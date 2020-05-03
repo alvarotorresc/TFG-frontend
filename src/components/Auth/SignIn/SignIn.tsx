d355d8ccbc2147277d4b3d6618fb237bf51438d7
alvarotorresc <alvarotorrescarrasco@gmail.com>
2020-05-03 19:04:29 +0200

    feat: refactor signin

--- src/components/Auth/SignIn/SignIn.tsx
+++ src/components/Auth/SignIn/SignIn.tsx
@@ -5,7 +5,7 @@ const LoginForm = () => (
   <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
     <Grid.Column style={{ maxWidth: 450 }}>
       <Header as="h2" color="teal" textAlign="center">
-        <Image src="logo192.png" /> Log-in to your account
+        <Image src="/logo192.png" /> Log-in to your account
       </Header>
       <Form size="large">
         <Segment stacked>
